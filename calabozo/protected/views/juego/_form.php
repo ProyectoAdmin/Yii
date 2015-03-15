64c592ea48608e8e8c98fe57672cf48ac577f1fd
RickTC <root@localhost.localdomain>
2015-03-15 16:03:34 -0600

    version con los cruds hechos

---
+++ calabozo/protected/views/juego/_form.php
@@ -0,0 +1,40 @@
+<?php
+/* @var $this JuegoController */
+/* @var $model Juego */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'juego-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'idJuego'); ?>
+		<?php echo $form->textField($model,'idJuego'); ?>
+		<?php echo $form->error($model,'idJuego'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'nombreJuego'); ?>
+		<?php echo $form->textField($model,'nombreJuego',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->error($model,'nombreJuego'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
